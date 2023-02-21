610c9c600ef018543c96b88d4a609a89a076e08a
jonjon <neumerance@live.com>
2023-02-21 15:02:29 +0800

    fix s3 object truncation

--- app/workers/import_cue_worker.rb
+++ app/workers/import_cue_worker.rb
@@ -2,11 +2,31 @@ class ImportCueWorker
   include Sidekiq::Worker
 
   def perform
-    s3 = Aws::S3::Client.new
-    resp = s3.list_objects(bucket: ENV['AWS_S3_LYRICS_BUCKET'])
+    resp = s3.list_objects_v2(bucket: ENV['AWS_S3_LYRICS_BUCKET'])
 
     resp.contents.each do |object|
       Cue.find_or_create_by(name: object.key)
     end
+  end
+
+  private
+
+  def import_cues(continuation_token = nil)
+    resp = s3.list_objects_v2(
+      bucket: ENV['AWS_S3_LYRICS_BUCKET'],
+      continuation_token: continuation_token
+    )
+
+    resp.contents.each do |object|
+      Cue.find_or_create_by(name: object.key)
+    end
+
+    if next_cont_token = resp.next_continuation_token
+      import_cues(next_cont_token)
+    end
+  end
+
+  def s3
+    @s3 = Aws::S3::Client.new
   end
 end
