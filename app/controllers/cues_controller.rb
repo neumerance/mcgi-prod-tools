5a4932c6f0647469b23e276279c0ccacf737103e
jonjon <neumerance@live.com>
2023-02-23 12:16:02 +0800

    add default lists

--- app/controllers/cues_controller.rb
+++ app/controllers/cues_controller.rb
@@ -1,9 +1,27 @@
 class CuesController < ApplicationController
+  DEFAULT_LINEUP = [
+    "001 - Fil, EN, BP, ES - CUT .jpg",
+    "MCGI (Kristiyano Ako, Kristiyano Tayo) - Fil, EN, BP, ES.jpg",
+    "Ama Salamat (Tanging Maiaalay).jpg",
+    "Araw ng Parangal at Pagpupugay (Updated).jpg",
+    "Papuri, Luwalhati (ADD anniv)- F.jpg",
+    "Alay sa Dakilang Tumawag.jpg",
+    "Tayo'y Magsiawit.jpg",
+    "Sagot sa Kabalisahan.jpg",
+    "Purihin at Pasalamatan - F.jpg",
+    "Ito ang Araw.jpg",
+    "Awit Ng Pagpapasalamat.jpg",
+    "Hallelujah, Amen - F.jpg",
+    "Stand Up - F.jpg", 
+    "Kaibigan.jpg",
+    "Obladi, Oblada.jpg"
+  ].freeze
+  
   def index
     cues =  if params[:keyword]
               Cue.where("lower(name) LIKE ?", "%#{URI.decode(params[:keyword].downcase)}%")
             else
-              Cue.limit(10)
+              Cue.where(name: DEFAULT_LINEUP)
             end
 
     render json: CueSerializer.new(cues).serialized_json
